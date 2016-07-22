8265fa5d4572c53c3706f31c29c205ec101aac4d
tommy crestanello <tommy.crestanello@digitaslbi.com>
2016-07-22 12:39:44 +0200

    meteo

--- routes/meteo.js
+++ routes/meteo.js
@@ -3,9 +3,20 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  if( req.body.location == "milano") {
-	  res.render('{"soleggiato"}');
-  }
+  	var location = rq.param('location');
+	if( location === 'milano') {
+		res.send('{"condizione":"soleggiato", "temperatura":"32°C"}');
+	}
+	if( location === 'padova') {
+		res.send('{"condizione":"variabile", "temperatura":"32°C"}');
+	}
+	if( location === 'milano') {
+		res.send('{"condizione":"nuvoloso", "temperatura":"22°C"}');
+	}
+	if( location === 'milano') {
+		res.send('{"condizione":"pioggie sparse", "temperatura":"18°C"}');
+	}
+	
 });
 
 module.exports = router;
