d2256f545b61cec41ea7415e1a2c37ff70176680
tommy crestanello <tommy.crestanello@digitaslbi.com>
2016-07-22 11:48:03 +0200

    meteo api

--- routes/meteo.js
+++ routes/meteo.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/meteo', function(req, res, next) {
+router.get('/', function(req, res, next) {
   res.render('index', { title: 'Meteo info json' });
 });
 
