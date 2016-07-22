74a4abb717d81378c763f1ea46b94bfd0033ac83
tommy crestanello <tommy.crestanello@digitaslbi.com>
2016-07-22 12:33:47 +0200

    meteo

--- routes/meteo.js
+++ routes/meteo.js
@@ -3,7 +3,9 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Meteo info json' });
+  if( req.body.location == "milano") {
+	  res.render('{"soleggiato"}');
+  }
 });
 
 module.exports = router;
